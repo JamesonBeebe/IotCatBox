7536ba54ed3529cb0f534b7aa1a784e16310e8d4
Jameson Beebe <10946222+JamesonBeebe@users.noreply.github.com>
2022-06-20 23:57:53 -0400

    -Made updates to server only when changes happen -Added more debug statements

--- src/main.ino
+++ src/main.ino
@@ -1,16 +1,25 @@
-//void app_main() {}
-// IoT Cat Litter Box V0.0 by Jameson Beebe
-// Measures Cat weight and frequency of use for multiple cats
+// void app_main() {}
+//  IoT Cat Litter Box V0.0 by Jameson Beebe
+//  Measures Cat weight and frequency of use for multiple cats
 
 /************************ Thingspeak  Configuration ************************/
 
 // visit thingspeak.com if you need to create an account or if you need your write key.
 #include "ThingSpeak.h"
-#include "secrets.h"
+#include "realsecrets.h"
 unsigned long myChannelNumber = SECRET_CH_ID;
 const char *apiKey = SECRET_WRITE_APIKEY;
 const char *readAPIKey = SECRET_READ_APIKEY;
 const char *server = "api.thingspeak.com"; //   "184.106.153.149" or api.thingspeak.com
+
+// Define Channels
+#define CAT_1_WEIGHT 1
+#define CAT_2_WEIGHT 2
+#define LID_STATUS 3
+#define SAND_WEIGHT 4
+#define CAT_1_USES 5
+#define CAT_2_USES 6
+#define POO_WEIGHT 7
 
 /************************ WIFI Configuration ************************/
 
@@ -19,7 +28,7 @@ const char *server = "api.thingspeak.com"; //   "184.106.153.149" or api.thingsp
 const char *ssid = SECRET_SSID; // replace with your wifi ssid and wpa2 key
 const char *pass = SECRET_PASS;
 WiFiClient client;
-#define WIFI_TIMEOUT 20 //Wi-Fi Timeout for local connections (multiply this number by 500mS)
+#define WIFI_TIMEOUT 20 // Wi-Fi Timeout for local connections (multiply this number by 500mS)
 
 /************************ OTA  Configuration ************************/
 #include <ESPAsyncTCP.h>
@@ -34,13 +43,15 @@ AsyncWebServer gui(2468);
 /************************ Define IO pins ************************/
 
 #include "HX711.h"
-#define calibration_factor -22994.0 //This value is obtained using the SparkFun_HX711_Calibration sketch
+#define calibration_factor -22994.0 // This value is obtained using the SparkFun_HX711_Calibration sketch
 #define DOUT 0                      // Pin connected to HX711 data output pin
 #define CLK 12                      // Pin connected to HX711 clk pin
 HX711 scale;                        // Scale initialization
 
 #define NUM_MEASUREMENTS 10 // Number of measurements
 #define THRESHOLD 0.2       // Restart averaging if the weight changes more than 0.2 kg.
+
+bool newData = false;
 
 float weight = 0;
 float sandWeight = 0;
@@ -55,13 +66,13 @@ float cat1MinWeightLbs = 8;    // expected minimum weight for cat #1
 float cat1MaxWeightLbs = 13;   // expected maximum weight for cat #1 in Lbs
 float cat2MinWeightLbs = 13.5; // expected minimum weight for cat #2 in Lbs
 
-long cat1Uses = 0;   // number of uses of the box in a day
-long cat2Uses = 0;   // number of uses of the box in a day
+uint8_t cat1Uses = 0;   // number of uses of the box in a day
+uint8_t cat2Uses = 0;   // number of uses of the box in a day
 float soilLevel = 0; // track soil level of box
 
 #define reed_switch_PIN 13
-int reed_switch_value = 1;
-int prev_reed_switch_value = 1;
+int reed_switch_value = digitalRead(reed_switch_PIN);
+int prev_reed_switch_value = reed_switch_value;
 
 /************************ Clock settings ************************/
 
@@ -71,7 +82,6 @@ int minutes;          // Current minute
 int seconds;          // Current second
 int seconds0;         // Last time the clock was updated
 #define TIMEZONE 4    // Current time zone. Used for correction of server time
-
 /********************** Local Defines ***************************/
 //#define __local_wifi__
 
@@ -80,6 +90,8 @@ String processor(const String &var);
 void zeroScale(void);
 void restartEsp(void);
 void resetScale(void);
+float getLastFieldValue(unsigned long channel);
+void readServerValues(void);
 
 void setup()
 {
@@ -92,7 +104,7 @@ void setup()
 
   scale.begin(DOUT, CLK); // Scale initialization
   // Scale calibration
-  scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
+  scale.set_scale(calibration_factor); // This value is obtained by using the SparkFun_HX711_Calibration sketch
 
   // Connect to WiFi router
   Serial.println("Connecting to ");
@@ -104,12 +116,12 @@ void setup()
     delay(500);
     Serial.print(".");
     if (count == WIFI_TIMEOUT)
-      break; //jump out of loop if local timeout hit to start SmartConfig
+      break; // jump out of loop if local timeout hit to start SmartConfig
     count++;
   }
 
   if (WiFi.status() != !WL_CONNECTED)
-  { //Local Auto-connect did not happen, start WiFi SmartConfig
+  { // Local Auto-connect did not happen, start WiFi SmartConfig
     WiFi.mode(WIFI_AP_STA);
     // Start WiFi Smart Config
     WiFi.beginSmartConfig();
@@ -136,7 +148,7 @@ void setup()
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
-  //Start OTA
+  // Start OTA
   otaServer.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
                { request->send(200, "text/plain", "Hi! I am ESP8266."); });
 
@@ -144,70 +156,47 @@ void setup()
   otaServer.begin();
   Serial.println("HTTP OTA server started");
 
-  //Start GUI
-  gui.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-         { request->send_P(200, "text/html", index_html, processor); });
-  gui.on("/zero", HTTP_GET, [](AsyncWebServerRequest *request)
-         { zeroScale(); });
-  gui.on("/reset", HTTP_GET, [](AsyncWebServerRequest *request)
-         { restartEsp(); });
-  gui.on("/resetscale", HTTP_GET, [](AsyncWebServerRequest *request)
-         { resetScale(); });
-  gui.begin();
 
-  //Start ThingSpeak interface
+    //Start GUI
+    gui.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+           { request->send_P(200, "text/html", index_html, processor); });
+    gui.on("/zero", HTTP_GET, [](AsyncWebServerRequest *request)
+           { zeroScale(); });
+    gui.on("/reset", HTTP_GET, [](AsyncWebServerRequest *request)
+           { restartEsp(); });
+    gui.on("/resetscale", HTTP_GET, [](AsyncWebServerRequest *request)
+           { resetScale(); });
+    gui.begin();
+
+  // Start ThingSpeak interface
   ThingSpeak.begin(client); // Initialize ThingSpeak
 
   // Read saved values
-  cat1Uses = ThingSpeak.readLongField(myChannelNumber, 5, readAPIKey);
-  cat2Uses = ThingSpeak.readLongField(myChannelNumber, 6, readAPIKey);
-  sandWeight = ThingSpeak.readLongField(myChannelNumber, 4, readAPIKey);
+  cat1Uses = (uint8_t)ThingSpeak.readLongField(myChannelNumber, CAT_1_USES, readAPIKey);
+  cat2Uses = (uint8_t)ThingSpeak.readLongField(myChannelNumber, CAT_2_USES, readAPIKey);
+  sandWeight = ThingSpeak.readFloatField(myChannelNumber, SAND_WEIGHT, readAPIKey);
 }
 
 void loop()
 {
   getTime(); // get current time
 
-  // restart daily parameters
-  if ((hours == 0) && (minutes == 0))
-  {
-    cat1Uses = 0;
-    ThingSpeak.setField(5, cat1Uses);
-    cat2Uses = 0;
-    ThingSpeak.setField(6, cat2Uses);
-  }
-
   // read Reed Switch
   reed_switch_value = digitalRead(reed_switch_PIN);
 
   // prepare to updated status of the box on Thingspeak if lid changed
+  if (reed_switch_value != prev_reed_switch_value)
+  {
+    ThingSpeak.setField(LID_STATUS, reed_switch_value);
+    newData = true;
+  }
 
-  if (reed_switch_value == 0)
-  {
-    ThingSpeak.setField(3, 1);
-  }
-  else
-  {
-    ThingSpeak.setField(3, 0);
-  }
-  /*
-  if((reed_switch_value==1) && (prev_reed_switch_value != reed_switch_value)){
-    ThingSpeak.setField(3, 0);
-  }
-  else if((reed_switch_value==0) && (prev_reed_switch_value!= reed_switch_value)){
-    ThingSpeak.setField(3, 1);
-  }
-*/
   // check if something changed
-  if ((reed_switch_value == 1) && (prev_reed_switch_value == 1))
-  {
-    Serial.println("Cleaning mode"); // On cleaning mode the controller will stop measuring the weight (do nothing)
-  }
   if ((reed_switch_value == 0) && (prev_reed_switch_value == 1))
   {
     Serial.println("Leaving cleaning mode"); // When it leaves cleaning mode, it shall wait some seconds and measure the weight of sand in the litter box
     delay(5000);
-    float weight0 = scale.get_units() - weightbox - platformWeight -soilLevel;
+    float weight0 = scale.get_units() - weightbox - platformWeight - soilLevel;
     float avgweight = 0;
     for (int i = 0; i <= NUM_MEASUREMENTS - 1; i++)
     { // Takes several measurements
@@ -225,15 +214,16 @@ void loop()
     Serial.print("Measured sand weight: ");
     Serial.print(sandWeight, 2);
     Serial.println(" lbs");
-    ThingSpeak.setField(4, sandWeight);
+    ThingSpeak.setField(SAND_WEIGHT, sandWeight);
 
-    //reset soil level after cleaning
+    // reset soil level after cleaning
     soilLevel = 0;
-    ThingSpeak.setField(7, soilLevel);
+    ThingSpeak.setField(POO_WEIGHT, soilLevel);
+    newData = true;
   }
-  if ((reed_switch_value == 0) && (prev_reed_switch_value == 0))
+  else if ((reed_switch_value == 0) && (prev_reed_switch_value == 0))
   {
-    Serial.println("Running mode"); // On running mode the controller will measure the weight of the cats
+    // On running mode the controller will measure the weight of the cats
     weight = scale.get_units() - weightbox - platformWeight - sandWeight - soilLevel;
 
     if (weight > cat1MinWeight)
@@ -254,23 +244,29 @@ void loop()
         weight0 = weight;
       }
       avgweight = avgweight / NUM_MEASUREMENTS; // Calculate average weight kg
-      avgweight = avgweight * 2.2;              //convert to lbs
+      avgweight = avgweight * 2.2;              // convert to lbs
       Serial.print("Measured weight: ");
       Serial.print(avgweight, 2);
       Serial.println(" lbs");
       if ((avgweight >= cat1MinWeightLbs) && (avgweight <= cat1MaxWeightLbs))
       { // cat #1 detected
-        Serial.println("Cat #1");
-        ThingSpeak.setField(1, avgweight); // set measured value to cat #1
-        cat1Uses += 1;
-        ThingSpeak.setField(5, cat1Uses);
+        ThingSpeak.setField(CAT_1_WEIGHT, avgweight); // set measured value to cat #1
+        cat1Uses++;
+        Serial.print("Cat #1 uses: ");
+        Serial.println(cat1Uses);
+
+        ThingSpeak.setField(CAT_1_USES, cat1Uses);
+        newData = true;
       }
-      if (avgweight >= cat2MinWeightLbs)
+      else if (avgweight >= cat2MinWeightLbs)
       { // cat #2 detected
-        Serial.println("Cat #2");
-        ThingSpeak.setField(2, avgweight); // set measured value to cat #2
-        cat2Uses += 1;
-        ThingSpeak.setField(6, cat2Uses);
+        ThingSpeak.setField(CAT_2_WEIGHT, avgweight); // set measured value to cat #2
+        cat2Uses++;
+        Serial.print("Cat #2 uses: ");
+        Serial.println(cat2Uses);
+        
+        ThingSpeak.setField(CAT_2_USES, cat2Uses);
+        newData = true;
       }
 
       // wait while there's someone on the scale
@@ -278,62 +274,72 @@ void loop()
       {
         delay(1000);
       }
-      //update the level of soil in the box
+      // update the level of soil in the box
       soilLevel = ((scale.get_units() - weightbox - platformWeight - sandWeight) * 2.2);
-      ThingSpeak.setField(7, soilLevel);
+      ThingSpeak.setField(POO_WEIGHT, soilLevel);
+      newData = true;
     }
   }
-
-  if ((reed_switch_value == 1) && (prev_reed_switch_value == 0))
+  else if ((reed_switch_value == 1) && (prev_reed_switch_value == 0))
   {
-    Serial.println("Leaving running mode"); // When it leaves running mode, it will stop measurint the weight (do nothing)
+    Serial.println("Cleaning Mode"); // When it leaves running mode, it will stop measurint the weight (do nothing)
+    zeroCatUses();
+  }
+  else {
+    ;
   }
 
-  // save values do Thingspeak
-  if (client.connect(server, 80))
-  {
-    int x = ThingSpeak.writeFields(myChannelNumber, apiKey);
-    switch (x)
+  if(newData){
+    gui.end();
+    // save values to Thingspeak only if something needs updating'
+    Serial.print("new data sending...");
+    if (client.connect(server, 80))
     {
-    case 200:
-      Serial.println("Success!");
-      break;
+      int x = ThingSpeak.writeFields(myChannelNumber, apiKey);
+      switch (x)
+      {
+      case 200:
+        Serial.println("Success!");
+        newData = false;
+        break;
 
-    case 404:
-      Serial.println("Incorrect API key or invalid ThingSpeak server address.");
+      case 404:
+        Serial.println("Incorrect API key or invalid ThingSpeak server address.");
 
-    case -301:
-      Serial.println("Failed to connect to ThingSpeak.");
+      case -301:
+        Serial.println("Failed to connect to ThingSpeak.");
 
-    case -304:
-      Serial.println("Timeout waiting for server to respond.");
+      case -304:
+        Serial.println("Timeout waiting for server to respond.");
 
-    default:
-      break;
+      default:
+        break;
+      }
     }
+    client.stop();
+    gui.begin();
   }
-  client.stop();
-  prev_reed_switch_value = reed_switch_value; // prepare for next scan
 
-  Serial.println("Waiting...");
-  delay(5000);
+  //Serial.println("Waiting...");
+  prev_reed_switch_value = reed_switch_value;
+  delay(100);
 }
 
-//Get current time Google server
+// Get current time Google server
 void getTime()
 {
 
   // connect server
   WiFiClient client;
-  while (!!!client.connect("google.com", 80))
+  while (!client.connect("google.com", 80))
   {
     Serial.println("connection failed, retrying...");
   }
   client.print("HEAD / HTTP/1.1\r\n\r\n");
 
-  delay(500); //wait a few milisseconds for incoming message
+  delay(500); // wait a few milisseconds for incoming message
 
-  //if there is an incoming message
+  // if there is an incoming message
   if (client.available())
   {
     while (client.available())
@@ -375,7 +381,7 @@ void getTime()
       }
     }
   }
-  //if no message was received (an issue with the Wi-Fi connection, for instance)
+  // if no message was received (an issue with the Wi-Fi connection, for instance)
   else
   {
     seconds = 0;
@@ -390,7 +396,7 @@ void getTime()
       }
     }
   }
-
+/*
   // serial output current time
   Serial.print("Current time: ");
   if (hours < 10)
@@ -404,6 +410,7 @@ void getTime()
     Serial.print("0");
   }
   Serial.println(minutes);
+  */
 }
 
 String readWeight(void)
@@ -413,71 +420,36 @@ String readWeight(void)
   float tempWeightBox = 0;
   float tempPlatformWeight = 0;
 
+  if (tempWeight >= platformWeight)
+  {
+    tempPlatformWeight = platformWeight;
 
-if(tempWeight>=platformWeight){
-  tempPlatformWeight = platformWeight;
-  
-  if(tempWeight>=(platformWeight+weightbox)){
-    tempWeightBox = weightbox;
+    if (tempWeight >= (platformWeight + weightbox))
+    {
+      tempWeightBox = weightbox;
 
-    if(tempWeight>=(platformWeight+weightbox+5))
-    tempSandWeight = sandWeight;
+      if (tempWeight >= (platformWeight + weightbox + 5))
+        tempSandWeight = sandWeight;
+    }
   }
-}
   Serial.print("Platform weight: ");
   Serial.println(tempPlatformWeight);
   Serial.print("Box weight: ");
   Serial.println(tempWeightBox);
   Serial.print("Litter weight: ");
   Serial.println(tempSandWeight);
-  
+
   Serial.print("Compensated weight: ");
-  tempWeight = (tempWeight - tempSandWeight - tempWeightBox - tempPlatformWeight)*2.205;
+  tempWeight = (tempWeight - tempSandWeight - tempWeightBox - tempPlatformWeight) * 2.205;
   Serial.println((tempWeight));
 
-    
-/*
-  //is there litter?
-  Serial.print("Litter weight: ");
-  if(tempWeight<(platformWeight+weightbox+1)){
-    tempSandWeight = 0;
-  }
-  else{
-    tempSandWeight = sandWeight;
-  }
-  Serial.println(tempSandWeight);
-
-  //is there a box?
-  Serial.print("Box weight: ");
-  if((platformWeight + weightbox)>tempWeight>=platformWeight){
-    tempWeightBox = 0;
-  }
-  else {
-    tempWeightBox = weightbox;
-  }
-  Serial.println(tempWeightBox);
-
-  //is there a platform?
-  Serial.print("Platform Weight: ");
-  if(tempWeight<platformWeight){
-    tempPlatformWeight = 0;
-  }
-  else{
-    tempPlatformWeight = platformWeight;
-  }
-  Serial.println(tempPlatformWeight);
-
-  Serial.print("compensated weight: ");
-  tempWeight = tempWeight - tempSandWeight - tempWeightBox - tempPlatformWeight;
-  Serial.println((tempWeight));
-*/
   return String(tempWeight);
 }
 
 // Replaces placeholder with DHT values
 String processor(const String &var)
 {
-  //Serial.println(var);
+  // Serial.println(var);
   if (var == "WEIGHT")
   {
     return readWeight();
@@ -487,7 +459,7 @@ String processor(const String &var)
 
 void zeroScale(void)
 {
-  scale.tare(); //reset the scale weight
+  scale.tare(); // reset the scale weight
 }
 
 void restartEsp()
@@ -500,6 +472,25 @@ void resetScale()
   scale.power_down();
   delay(250);
   scale.power_up();
-  //scale.begin(DOUT, CLK);
+  // scale.begin(DOUT, CLK);
   scale.set_scale(calibration_factor);
+}
+
+float getLastFieldValue(unsigned long field)
+{
+  return ThingSpeak.readFloatField(myChannelNumber, field);
+}
+
+void readServerValues(void)
+{
+  // Read saved values
+  cat1Uses = ThingSpeak.readLongField(myChannelNumber, CAT_1_USES, readAPIKey);
+  cat2Uses = ThingSpeak.readLongField(myChannelNumber, CAT_2_USES, readAPIKey);
+  sandWeight = ThingSpeak.readLongField(myChannelNumber, SAND_WEIGHT, readAPIKey);
+}
+
+void zeroCatUses(void)
+{
+  cat1Uses = 0;
+  cat2Uses = 0;
 }
