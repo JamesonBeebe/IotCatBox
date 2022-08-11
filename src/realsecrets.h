b54d3de419e9bf33f07032387b63cb1c94af6760
Jameson Beebe <10946222+JamesonBeebe@users.noreply.github.com>
2022-08-11 19:04:23 -0400

    removed keys

--- src/realsecrets.h
+++
@@ -1,9 +0,0 @@
-// Use this file to store all of the private credentials 
-// and connection details
-
-#define SECRET_SSID "whiskey"                                // replace MySSID with your WiFi network name
-#define SECRET_PASS "caramelcat"                            // replace MyPassword with your WiFi password
-
-#define SECRET_CH_ID 1357181                                 // replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "DGISBWVL513DD9M4"                           // replace XYZ with your channel write API Key
-#define SECRET_READ_APIKEY "EAL7YI5W5NF0Q8SQ"
